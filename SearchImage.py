826cab862ce00479f0b0fc2c1c234d7647bfe31e
pingouInfini <pingouInfini@banquise.com>
2019-10-11 17:54:09 +0200

    Adaptation des xpath et extract url pour GoogleImage

--- SearchImage.py
+++ SearchImage.py
@@ -1,23 +1,14 @@
-import googlesearch
-from Search import Search
 import logging
+import json
+
+from Search import Search
 
 
 class SearchImage(Search):
-    def search(self, query, number, standard):
-        logging.info("Recherche google image : " + query)
+    def search(self, query, number):
+        logging.info("Recherche google news : " + query)
+        xpath = "//div[contains(@class,'rg_meta')]"
+        return Search.get_urls(self, query, number, xpath)
 
-        racineURL = 'https://www.google.co.in/search?q={}&start={}&tbm=isch&num={}&filter=0'
-
-        # on s'appuie sur les resultats renvoyé par google search et non ceux de googles images pour de meilleurs résultats
-        return googlesearch.search(
-            query,
-            tld="fr",
-            lang="fr",
-            num=50,
-            start=0,
-            stop=int(number),
-            pause=2,
-            # (tpe = search type) : Use the following values {videos: ‘vid’, images: ‘isch’, news: ‘nws’, shopping: ‘shop’, books: ‘bks’, applications: ‘app’}
-            tpe='isch',
-            only_standard=(standard == "True"))
+    def extract_url_from_link(self, link):
+        return json.loads(link.get_attribute('innerHTML'))["ru"]
